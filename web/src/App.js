57e24add666a2080ff790969bde9678ef60f396b
David Lima <davidlimalourenco@gmail.com>
2020-01-17 14:54:43 -0300

    web - added axios and search dev functionality
    Neste commit conectei o frontend com o back end para que o cadastro de devs funcionasse.

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([])
+
+  const [github_username, setGithub_Username] = useState('')
+  const [techs, setTechs] = useState('')
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+
+      (err) => {
+        console.log(err)
+      },
+
+      {
+        timeout: 30000,
+      }
+
+    )
+  }, [])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const res = await api.get('/devs')
+
+      setDevs(res.data)
+    }
+
+    loadDevs()
+  }, [])
+  
+  async function handleAddDev(e) {
+    e.preventDefault()
+
+    const res = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithub_Username('')
+    setTechs('')
+
+    setDevs([...devs, res.data])
+  }  
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+            name="github_username"
+            id="github_username"
+            required
+            value={github_username}
+            onChange={e => setGithub_Username(e.target.value)}
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+            name="techs"
+            id="techs"
+            required
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input type="number"
+              name="latitude"
+              id="latitude"
+              required value={latitude}
+              onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input 
+              type="number"
+              name="longitude"
+              id="longitude"
+              required value={longitude}
+              onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
           <button type="submit">Salvar</button>
@@ -37,54 +114,20 @@ function App() {
       </aside>
       <main>
       <ul>
-          <li className="dev-item">
+        {devs.map(dev => (
+          <li key={dev._id}className="dev-item">
             <header>
-              <img src="https://avatars1.githubusercontent.com/u/32413509?s=400&v=4" alt="David Lima"></img>
-              <div className="user info">
-                <strong>David Lima</strong>
-                <span>ReactJS, ReactNative, Node.js</span>
+              <img src={dev.avatar_url} alt={dev.name}></img>
+              <div className="user-info">
+                <strong>{dev.name}</strong>
+                <span>{dev.techs.join(', ')}</span>
               </div>
             </header>
-            <p>Student and code lover</p>
-            <a href="https://github.com/dlourennco">Acessar perfil no Github</a>
+            <p>{dev.bio}</p>
+            <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
           </li>
-          
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/32413509?s=400&v=4" alt="David Lima"></img>
-              <div className="user info">
-                <strong>David Lima</strong>
-                <span>ReactJS, ReactNative, Node.js</span>
-              </div>
-            </header>
-            <p>Student and code lover</p>
-            <a href="https://github.com/dlourennco">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/32413509?s=400&v=4" alt="David Lima"></img>
-              <div className="user info">
-                <strong>David Lima</strong>
-                <span>ReactJS, ReactNative, Node.js</span>
-              </div>
-            </header>
-            <p>Student and code lover</p>
-            <a href="https://github.com/dlourennco">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/32413509?s=400&v=4" alt="David Lima"></img>
-              <div className="user info">
-                <strong>David Lima</strong>
-                <span>ReactJS, ReactNative, Node.js</span>
-              </div>
-            </header>
-            <p>Student and code lover</p>
-            <a href="https://github.com/dlourennco">Acessar perfil no Github</a>
-          </li>
-        </ul>
+        ))}
+      </ul>
       </main>
     </div>
   );
